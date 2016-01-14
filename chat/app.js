ccb6ac26a74ff34e9f3780fccfdec276e960ec44
Derek <dmalvarado@gmail.com>
2016-01-13 21:04:30 -0600

    restructure into chat, api, ui

--- chat/app.js
+++ chat/app.js
@@ -34,14 +34,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-//enable cors
-app.use(function(req,res,next){
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 // error handlers
 
 // development error handler
